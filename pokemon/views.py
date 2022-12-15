aa955a83d8edf985f068b1e2a1bf48c0894a0de2
Arthur21x <86072531+Arthur21x@users.noreply.github.com>
2022-12-14 23:23:12 -0300

    barra de pesquisa

--- pokemon/views.py
+++ pokemon/views.py
@@ -7,14 +7,19 @@ import requests
 # Create your views here.
 def index(request):
     Pokemons = PokemonInfo.objects.all()
-    paginator = Paginator(Pokemons, 25)
+    paginator = Paginator(Pokemons, 80)
 
     page = request.GET.get('page')
     Pokemons = paginator.get_page(page)
     return render(request, 'pokemon/index.html', {'Pokemon': Pokemons})
 
 
-def pokemon(request, identificador):
-    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{identificador}').json()
-    return render(request, 'pokemon/pokemon.html', {'response', response})
+def busca(request):
+    pokemon = request.GET.get('pokemon')
+    Pokemons = PokemonInfo.objects.order_by('-id').filter(name=pokemon.lower())
+    paginator = Paginator(Pokemons, 80)
+
+    page = request.GET.get('page')
+    Pokemons = paginator.get_page(page)
+    return render(request, 'pokemon/busca.html', {'Pokemon': Pokemons})
 
